1c118c222a4306b723d0135c9bfbe79932a282ce
ahmad-mhajna <ahmad.mhmad.rashed777@gmail.com>
2022-06-08 16:40:22 +0300

    api fix

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const apiInstance = axios.create({
-  baseURL: "https://6294b28763b5d108c19197d9.mockapi.io/shoes",
+  baseURL: "https://6299bd076f8c03a978485945.mockapi.io/shoes",
 });
 export default apiInstance;
